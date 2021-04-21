8024019e9e89076b51abafcd4df857468b55d94e
LightRid <lightrid@gmail.com>
2021-04-21 19:11:10 +0300

    Start commit

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        print("The value of the slider is now: \(sender.value)")
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
